d8151a319ce4fabeebc80d944c951b44ed04739c
EC2 Default User <ec2-user@ip-172-31-71-240.ec2.internal>
2020-10-24 23:29:00 +0000

    adding img file and about html

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title> Lab 1: Personal Page </title>
+        <meta charset="utf-8">
     </head>
     <body>
         
+        <header>
+            <h1> Jordan Cruz </h1>
+        </header>
         
-        <h1>Welcome to my website!</h1>
+        <hr>
+        
+        <nav>
+            
+            <a href="index.html"> Home </a>
+            <a href="about.html"> About </a>
+            <a href="contact.html"> Contact </a>
+            
+        </nav>
+        
+        <main>
+            
+            <firgure>
+                <img src="img_p1/juan_doe.png">
+            </firgure>
+            
+            <div>
+                Welcome to my site!
+            </div>
+            
+        </main>
+        
+        <footer>
+            
+            
+            
+        </footer>
+
 
     </body>
 </html>
